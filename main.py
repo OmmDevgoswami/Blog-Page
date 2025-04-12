4310330534e8dc364c8130e478927f4aafedd3ea
OmmDevgoswami <teamuniverse.omm@gmail.com>
2025-04-12 20:03:53 +0530

    PostgreSQL added as the DB

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ app.jinja_env.globals['avatar'] = dicebear_avatar
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///new_posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRESQL_DATABASE', 'sqlite:///new_posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
